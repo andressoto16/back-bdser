87ab037acbd297f18507f37e7bcb040f4a90f9e4
Ecosistema-UNP <157920627+andressoto16@users.noreply.github.com>
2024-11-18 08:06:59 -0500

    archivo readme para la instalación de librerias

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Usuario\OneDrive\Escritorio\base-datos-ser\env
+set VIRTUAL_ENV=C:\Users\fabian.soto\Desktop\Proyectos\back-bdser\env
 
 if not defined PROMPT set PROMPT=$P$G
 
